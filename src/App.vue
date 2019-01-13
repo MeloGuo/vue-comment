4374c7a42c5324640511cb37a89c70415fcb7e76
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 22:07:04 +0800

    运行组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    
+    <comment-app></comment-app>
   </div>
 </template>
 
 <script>
+import CommentApp from './containers/CommentApp.vue'
+import './index.css'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: { CommentApp }
 }
 </script>

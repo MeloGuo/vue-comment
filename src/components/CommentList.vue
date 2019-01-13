49430678a69eff57c7c5264809fe4edb06d12ee4
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 22:04:29 +0800

    完成 CommentList 组件

---
+++ src/components/CommentList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <template v-for="(comment, index) in comments">
+      <comment
+        :comment="comment" 
+        :key="index" 
+        :index="index"
+        @delete:comment="onDeleteComment"></comment>
+    </template>
+  </div>
+</template>
+
+<script>
+import Comment from './Comment.vue'
+
+export default {
+  name: 'CommentList',
+  components: { Comment },
+  props: {
+    comments: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    onDeleteComment (index) {
+      this.$emit('delete:comment', index)
+    }
+  }
+}
+</script>

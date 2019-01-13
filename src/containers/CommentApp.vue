ca9d14c185c8324813a4f5d7d0ffbea350ecdc98
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 22:06:29 +0800

    完成 CommentAPP 组件

---
+++ src/containers/CommentApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <comment-input></comment-input>
+    <comment-list></comment-list>
+  </div>
+</template>
+
+<script>
+import CommentInput from './CommentInput.vue'
+import CommentList from './CommentList.vue'
+
+export default {
+  name: 'CommentApp',
+  components: {
+    CommentInput,
+    CommentList
+  }
+}
+</script>

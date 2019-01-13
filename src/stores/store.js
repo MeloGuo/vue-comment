f4c1057e5478a4331e20ad567b46adaa143bb6c7
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 20:49:37 +0800

    完成 store.js

--- src/stores/store.js
+++ src/stores/store.js
@@ -3,12 +3,28 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// mutation-types
+export const INIT_COMMENTS = 'INIT_COMMENTS'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+
 export default new Vuex.Store({
   state: {
-
+    comments: []
   },
   mutations: {
-
+    [INIT_COMMENTS] (state, { comments }) {
+      state.comments = comments
+    },
+    [ADD_COMMENT] (state, { comment }) {
+      state.comments = [...state.comments, comment]
+    },
+    [DELETE_COMMENT] (state, { index }) {
+      state.comments = [
+        ...state.comments.slice(0, index),
+        ...state.comments.slice(index + 1)
+      ]
+    }
   },
   actions: {
 

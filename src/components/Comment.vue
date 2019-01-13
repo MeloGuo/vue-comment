6173c6351f537051d48f5b72a29643dda3b83e09
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 22:03:49 +0800

    完成 Comment 组件

---
+++ src/components/Comment.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="comment">
+    <div class="comment-user">
+      <span>{{comment.username}}</span>
+    </div>
+    <p>{{comment.content}}</p>
+    <span class="comment-createdtime">
+      {{timeString}}
+    </span>
+    <span class="comment-delete" @click="handleDeleteComment">
+      删除
+    </span>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  props: {
+    comment: {
+      type: Object
+    },
+    index: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+      timeString: ''
+    }
+  },
+  methods: {
+    created () {
+      this._updateTimeString()
+      this._timer = setInterval(() => {
+        this._updateTimeString()
+      }, 5000)
+    },
+    beforeDestroy () {
+      clearInterval(this._timer)
+    },
+    _updateTimeString () {
+      const comment = this.comment
+      const duration = (+Date.now() - comment.createdTime) / 1000
+      this.timeString = duration > 60
+        ? `${Math.round(duration / 60)} 分钟前`
+        : `${Math.round(Math.max(duration, 1))} 秒前`
+    },
+    handleDeleteComment () {
+      this.$emit('delete:comment', this.index)
+    }
+  }
+}
+</script>
+

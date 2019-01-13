f699577aec3e27c622e11afa6aae6f2da353fd63
MeloGuo <guoziliang199606@gmail.com>
2019-01-13 22:06:07 +0800

    完成 CommentInputContainer 组件

---
+++ src/containers/CommentInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <comment-input
+    :username="username"
+    @username:blur="_saveUsername"
+    @submit="handleSubmitComment"></comment-input>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+import { ADD_COMMENT } from '../stores/store'
+import CommentInput from '../components/CommentInput.vue'
+
+export default {
+  name: 'CommentInputContainer',
+  components: { CommentInput },
+  data () {
+    return {
+      username: ''
+    }
+  },
+  computed: mapState({
+    comments: state => state.comments
+  }),
+  methods: {
+    ...mapMutations({
+      addComment: ADD_COMMENT
+    }),
+    created () {
+      this._loadUsername()
+    },
+    _loadUsername () {
+      const username = window.localStorage.getItem('username')
+      if (username) {
+        this.username = username
+      }
+    },
+    _saveUsername (username) {
+      window.localStorage.setItem('username', username)
+    },
+    handleSubmitComment (comment) {
+      if (!comment) {
+        return
+      }
+      if (!comment.username) {
+        return window.alert('Please enter username!')
+      }
+      if (!comment.content) {
+        return window.alert('Please enter content')
+      }
+      const newComments = [...this.comments, comment]
+      window.localStorage.setItem('comments', JSON.stringify(newComments))
+      this.addComment({ comment }) // this.$store.commit(ADD_COMMENT, { comment })
+    }
+  }
+}
+</script>
+
